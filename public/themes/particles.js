effa61d9fbb713d441a8e543b6c192317462a781
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:39:31 -0500

    Project Auto | Repo modified public/themes/particles.js

--- public/themes/particles.js
+++ public/themes/particles.js
@@ -1,21 +1,9 @@
-FILE PATH: public/index.html
-CONTENT: ```html
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Web Proxy</title>
-  <link rel="stylesheet" href="/themes/particles.css">
-  <script src="/themes/particles.js"></script>
-</head>
-<body>
-  <div id="particles-js"></div>
-  <script>
-    particlesJS.load('particles-js', 'themes/particles.json', function() {
-      console.log('callback - particles.js config loaded');
-    });
-  </script>
-</body>
-</html>
+FILE PATH: public/themes/particles.css
+CONTENT: ```css
+/* Particles CSS */
+#particles-js {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+}
 ```
