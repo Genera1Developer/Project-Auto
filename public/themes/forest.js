9902ada064d17a1c4b625653615433e5da1d84b8
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:55:22 -0500

    Project Auto | Repo modified public/themes/forest.js

--- public/themes/forest.js
+++ public/themes/forest.js
@@ -1,35 +1,35 @@
-Based on the project goal, what file should be created? Provide the file path and content in the following format:
-FILE PATH: public/index.html
+FILE PATH: public/script.js
 CONTENT: 
-```html
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Web Proxy</title>
-  <link rel="stylesheet" href="/themes/forest.css">
-  <script src="/themes/forest.js"></script>
-</head>
-<body>
-  <nav>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
-  <main>
-    <h1>Welcome to the Web Proxy!</h1>
-    <p>This is a simple web proxy that allows you to browse the web anonymously.</p>
-    <p>To use the proxy, simply enter the URL of the website you want to visit in the form below.</p>
-    <form>
-      <input type="text" name="url">
-      <input type="submit" value="Go">
-    </form>
-  </main>
-  <footer>
-    <p>Copyright 2023 Web Proxy</p>
-  </footer>
-</body>
-</html>
+```javascript
+const urlInput = document.querySelector('input[name="url"]');
+const submitButton = document.querySelector('input[type="submit"]');
 
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+
+  const url = urlInput.value;
+
+  if (!url) {
+    alert('Please enter a URL.');
+    return;
+  }
+
+  // Send the URL to the server to be proxied.
+  fetch('/proxy', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url }),
+  })
+    .then(res => res.json())
+    .then(data => {
+      // Display the proxied content in the browser.
+      document.body.innerHTML = data.content;
+    })
+    .catch(err => {
+      alert('An error occurred while proxying the URL.');
+      console.error(err);
+    });
+});
 ```
