97ce436e04a7f7a366a1bccfbf3b8a59b768ee15
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:44:28 -0500

    Project Auto | Repo modified public/themes/particles.js

--- public/themes/particles.js
+++ public/themes/particles.js
@@ -1,7 +1,58 @@
-FILE PATH: public/themes/particles.css
-CONTENT: ```css
-#particles-js {
-  width: 100%;
-  position: absolute;
-}
+FILE PATH: public/themes/particles.js
+CONTENT: ```javascript
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 150, density: { enable: true, value_area: 800 } },
+    color: { value: "#808080" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#808080",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "bounce",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
 ```
