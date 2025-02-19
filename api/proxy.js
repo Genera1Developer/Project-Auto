4076172f5c3e4530cf0489a1a1c034c7be704505
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:13:38 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,10 +4,13 @@ content:
 const express = require("express");
 const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(
   "/proxy",
