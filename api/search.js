de0dfe2930356ff2bc0e18edb06734efddf241f5
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:56:16 -0500

    Project Auto | Repo modified api/search.js

--- api/search.js
+++ api/search.js
@@ -1,17 +1,9 @@
-Based on the project goal, here is the file:
-
-FILE PATH: api/search.js
+FILE PATH: templates/head.html
 CONTENT: 
-```javascript
-const fetch = require("node-fetch");
-
-module.exports = async (req, res) => {
-  const q = req.query.q;
-
-  const results = await fetch(
-    `https://www.google.com/search?q=${q}&output=json`
-  ).then((res) => res.json());
-
-  res.json({ results });
-};
+```html
+<head>
+  <title>Web Proxy</title>
+  <link rel="stylesheet" href="/style.css" />
+  <script src="/script.js"></script>
+</head>
 ```
