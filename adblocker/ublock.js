a0ce736b942804c780eeb46ebc1894270d5aab24
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:03:49 -0500

    Project Auto | Repo modified adblocker/ublock.js

--- adblocker/ublock.js
+++ adblocker/ublock.js
@@ -18,8 +18,17 @@ content:
 
           // Check if the node is a script tag
           if (node.nodeName.toLowerCase() === 'script') {
-            // Remove the script tag
-            node.parentNode.removeChild(node);
+            // Check if the script tag has a src attribute
+            if (node.hasAttribute('src')) {
+              // Get the src attribute value
+              var src = node.getAttribute('src');
+
+              // Check if the src attribute value contains a known ad domain
+              if (src.includes('doubleclick.net') || src.includes('adx.g.doubleclick.net') || src.includes('googlesyndication.com') || src.includes('facebook.com') || src.includes('outbrain.com') || src.includes('taboola.com')) {
+                // Remove the script tag
+                node.parentNode.removeChild(node);
+              }
+            }
           }
         }
       }
@@ -33,3 +42,10 @@ content:
   });
 })();
 ```
+
+The following files are also necessary for a fully functional web proxy:
+
+- server.js: This file will contain the necessary code to run the web proxy server.
+- index.html: This file will contain the HTML code for the web proxy interface.
+- styles.css: This file will contain the CSS code for the web proxy interface.
+- README.md: This file will contain the documentation for the web proxy project.
