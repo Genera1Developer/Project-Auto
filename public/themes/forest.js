82876612fb9651387db6aba8249a4fef55296358
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:53:59 -0500

    Project Auto | Repo modified public/themes/forest.js

--- public/themes/forest.js
+++ public/themes/forest.js
@@ -1,159 +1,35 @@
-FILE PATH: public/themes/forest.js
+Based on the project goal, what file should be created? Provide the file path and content in the following format:
+FILE PATH: public/index.html
 CONTENT: 
-```javascript
-// This file contains the JavaScript code for the forest theme.
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Web Proxy</title>
+  <link rel="stylesheet" href="/themes/forest.css">
+  <script src="/themes/forest.js"></script>
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  <main>
+    <h1>Welcome to the Web Proxy!</h1>
+    <p>This is a simple web proxy that allows you to browse the web anonymously.</p>
+    <p>To use the proxy, simply enter the URL of the website you want to visit in the form below.</p>
+    <form>
+      <input type="text" name="url">
+      <input type="submit" value="Go">
+    </form>
+  </main>
+  <footer>
+    <p>Copyright 2023 Web Proxy</p>
+  </footer>
+</body>
+</html>
 
-function changeThemeToForest() {
-  // Change the background color of the body.
-  document.body.style.backgroundColor = "#388e3c";
-
-  // Change the font family of the body.
-  document.body.style.fontFamily = "Arial, sans-serif";
-
-  // Change the color of all headings.
-  var headings = document.getElementsByTagName("h1");
-  for (var i = 0; i < headings.length; i++) {
-    headings[i].style.color = "#ffffff";
-  }
-
-  // Change the color of all paragraphs.
-  var paragraphs = document.getElementsByTagName("p");
-  for (var i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "#ffffff";
-  }
-
-  // Change the color of all links.
-  var links = document.getElementsByTagName("a");
-  for (var i = 0; i < links.length; i++) {
-    links[i].style.color = "#ffffff";
-    links[i].style.textDecoration = "none";
-  }
-
-  // Change the background color of the navigation bar.
-  var nav = document.getElementsByTagName("nav");
-  for (var i = 0; i < nav.length; i++) {
-    nav[i].style.backgroundColor = "#2e7d32";
-  }
-
-  // Change the display of the navigation bar ul.
-  var navUl = document.getElementsByTagName("nav ul");
-  for (var i = 0; i < navUl.length; i++) {
-    navUl[i].style.display = "flex";
-    navUl[i].style.justifyContent = "space-between";
-  }
-
-  // Change the padding of the navigation bar li.
-  var navLi = document.getElementsByTagName("nav li");
-  for (var i = 0; i < navLi.length; i++) {
-    navLi[i].style.padding = "10px";
-  }
-
-  // Change the color of the navigation bar a.
-  var navA = document.getElementsByTagName("nav a");
-  for (var i = 0; i < navA.length; i++) {
-    navA[i].style.color = "#ffffff";
-    navA[i].style.textDecoration = "none";
-  }
-}
-```
-
-FILE PATH: public/themes/desert.css
-CONTENT: 
-```css
-/* This file contains the CSS code for the desert theme. */
-
-body {
-  background-color: #ffe4c4;
-  font-family: Arial, sans-serif;
-}
-
-h1, h2, h3, h4, h5, h6 {
-  color: #000000;
-}
-
-p {
-  color: #000000;
-}
-
-a {
-  color: #000000;
-  text-decoration: none;
-}
-
-nav {
-  background-color: #ffd166;
-}
-
-nav ul {
-  display: flex;
-  justify-content: space-between;
-}
-
-nav li {
-  padding: 10px;
-}
-
-nav a {
-  color: #000000;
-  text-decoration: none;
-}
-```
-
-FILE PATH: public/themes/desert.js
-CONTENT: 
-```javascript
-// This file contains the JavaScript code for the desert theme.
-
-function changeThemeToDesert() {
-  // Change the background color of the body.
-  document.body.style.backgroundColor = "#ffe4c4";
-
-  // Change the font family of the body.
-  document.body.style.fontFamily = "Arial, sans-serif";
-
-  // Change the color of all headings.
-  var headings = document.getElementsByTagName("h1");
-  for (var i = 0; i < headings.length; i++) {
-    headings[i].style.color = "#000000";
-  }
-
-  // Change the color of all paragraphs.
-  var paragraphs = document.getElementsByTagName("p");
-  for (var i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "#000000";
-  }
-
-  // Change the color of all links.
-  var links = document.getElementsByTagName("a");
-  for (var i = 0; i < links.length; i++) {
-    links[i].style.color = "#000000";
-    links[i].style.textDecoration = "none";
-  }
-
-  // Change the background color of the navigation bar.
-  var nav = document.getElementsByTagName("nav");
-  for (var i = 0; i < nav.length; i++) {
-    nav[i].style.backgroundColor = "#ffd166";
-  }
-
-  // Change the display of the navigation bar ul.
-  var navUl = document.getElementsByTagName("nav ul");
-  for (var i = 0; i < navUl.length; i++) {
-    navUl[i].style.display = "flex";
-    navUl[i].style.justifyContent = "space-between";
-  }
-
-  // Change the padding of the navigation bar li.
-  var navLi = document.getElementsByTagName("nav li");
-  for (var i = 0; i < navLi.length; i++) {
-    navLi[i].style.padding = "10px";
-  }
-
-  // Change the color of the navigation bar a.
-  var navA = document.getElementsByTagName("nav a");
-  for (var i = 0; i < navA.length; i++) {
-    navA[i].style.color = "#000000";
-    navA[i].style.textDecoration = "none";
-  }
-}
 ```
