4b20945d933d1eb62584f4cc617800e3dd9c82c6
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:54:53 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,24 @@
-Based on the project goal, what file should be created? Provide the file path and content in the following format:
-FILE PATH: views/index.html
+FILE PATH: server.js
 CONTENT: 
-```html
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Web Proxy</title>
-    <link rel="stylesheet" href="/styles.css">
-</head>
-<body>
-    <h1>Web Proxy</h1>
-    <p>Enter the URL of the website you want to visit:</p>
-    <form action="/proxy" method="POST">
-        <input type="text" name="url" placeholder="URL">
-        <input type="submit" value="Go">
-    </form>
-    <div id="result"></div>
-</body>
-</html>
+```javascript
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', { root: __dirname });
+});
+
+app.post('/proxy', async (req, res) => {
+  const { url } = req.body;
+  const { getHtml } = require('./utils');
+  const html = await getHtml(url);
+  res.send(html);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 ```
