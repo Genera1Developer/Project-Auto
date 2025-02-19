e6f2c0e91fabbcfec9c0ff019324f2ee0dd7933f
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:47:11 -0500

    Project Auto | Repo modified public/themes/forest.js

--- public/themes/forest.js
+++ public/themes/forest.js
@@ -1,18 +1,31 @@
-FILE PATH: public/themes/forest.js
+Based on the project goal, what file should be created? Provide the file path and content in the following format:
+FILE PATH: public/components/Menu.js
 CONTENT: 
 ```js
-const theme = {
-  colors: {
-    primary: '#008000',
-    secondary: '#808000',
-    background: '#ffffff',
-    text: '#000000',
-    link: '#0000ff',
-  },
-   fonts: {
-    primary: 'Helvetica',
-  },
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    fetch('api/menu-items')
+      .then(res => res.json())
+      .then(data => setMenuItems(data));
+  }, []);
+
+  return (
+    <nav>
+      <ul>
+        {menuItems.map(item => (
+          <li key={item.id}>
+            <Link to={item.url}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
-export default theme;
+export default Menu;
 ```
