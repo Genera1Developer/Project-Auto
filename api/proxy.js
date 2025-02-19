187fa5c4ac6f4b089b21362a6f7e69e976bbd5f2
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:45:44 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,32 +1,18 @@
-Based on the project goal, what file should be created? Provide the file path and content in the following format:
-FILE PATH: api/proxy.js
+FILE PATH: public/index.html
 CONTENT: 
-```javascript
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-
-const targetUrl = 'https://example.com'; // Change this to the target URL you want to proxy requests to
-
-const proxyServer = http.createServer((req, res) => {
-  const options = {
-    hostname: targetUrl,
-    port: 443,
-    path: req.url,
-    method: req.method,
-    headers: req.headers,
-  };
-
-  const proxyReq = https.request(options, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    proxyRes.pipe(res);
-  });
-
-  req.pipe(proxyReq);
-});
-
-proxyServer.listen(3000, () => {
-  console.log('Proxy server listening on port 3000');
-});
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Web Proxy</title>
+</head>
+<body>
+  <h1>Web Proxy</h1>
+  <p>Enter the URL you want to access below:</p>
+  <form action="/proxy" method="POST">
+    <input type="text" name="url" placeholder="URL">
+    <input type="submit" value="Go">
+  </form>
+</body>
+</html>
 ```
