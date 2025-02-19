76a05f071f4c259d16a84fc5a49e3c17b5af19ef
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:09:15 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -71,36 +71,9 @@ app.get('/api/proxy', async (req, res) => {
   }
 });
 
-app.use(express.static('public'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(port, () => {
   console.log(`Proxy server running on http://localhost:${port}`);
 });
-
-```
-file path: package.json
-content: ```json
-{
-  "name": "proxy",
-  "version": "1.0.0",
-  "description": "Web Proxy Project",
-  "main": "app.js",
-  "scripts": {
-    "start": "node app.js",
-    "dev": "nodemon app.js"
-  },
-  "dependencies": {
-    "axios": "^0.21.1",
-    "express": "^4.17.1",
-    "express-async-errors": "^4.1.1",
-    "express-rate-limit": "^5.4.1",
-    "path": "^0.12.7"
-  }
-}
 ```
 file path: app.js
 content: ```javascript
@@ -124,3 +97,66 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 ```
+file path: package-lock.json
+content: ```json
+{
+  "name": "proxy",
+  "version": "1.0.0",
+  "lockfileVersion": 1,
+  "requires": true,
+  "dependencies": {
+    "@babel/code-frame": {
+      "version": "7.12.11",
+      "resolved": "https://registry.npmjs.org/@babel/code-frame/-/code-frame-7.12.11.tgz",
+      "integrity": "sha512-Zt1yodBx1UcyiePMSkWnU4hPqhwq7hGi2nFL1LeA3EUl+q2LQx16MISgJ0+z7dnmgvP9QtIleuETGOiUX1f2sQ==",
+      "dev": true,
+      "requires": {
+        "@babel/highlight": "^7.10.4"
+      }
+    },
+    "@babel/generator": {
+      "version": "7.12.11",
+      "resolved": "https://registry.npmjs.org/@babel/generator/-/generator-7.12.11.tgz",
+      "integrity": "sha512-HKlu93x0yHVO66xfz/t3z60ex/D33aA4X8ih3s/t102hvfpAO7nPiRDa5tAQ2kj9sEMv91dGMolGOKXGrnUTQ==",
+      "dev": true,
+      "requires": {
+        "@babel/types": "^7.12.11",
+        "jsesc": "^2.5.1",
+        "source-map": "^0.5.0"
+      }
+    },
+    "@babel/helper-annotate-as-pure": {
+      "version": "7.12.10",
+      "resolved": "https://registry.npmjs.org/@babel/helper-annotate-as-pure/-/helper-annotate-as-pure-7.12.10.tgz",
+      "integrity": "sha512-XplmVbC1n+Ky6jSKK8qIx4T4BoP/g2E5gJh8I4O8r8k+d+GlKP3rcl/t3QmfN9bRco+TLPDMyZgSpPXp9AhVw==",
+      "dev": true,
+      "requires": {
+        "@babel/types": "^7.12.10"
+      }
+    },
+    "@babel/helper-builder-binary-assignment-operator-visitor": {
+      "version": "7.12.13",
+      "resolved": "https://registry.npmjs.org/@babel/helper-builder-binary-assignment-operator-visitor/-/helper-builder-binary-assignment-operator-visitor-7.12.13.tgz",
+      "integrity": "sha512-CZOv9tGphhYs7PGn/odDI8cL9322/V0gHpk2W3a27zhoENXgJG3TqG80/ctxhqz8b5alsG8hyX8i+W4qi3+2Ug==",
+      "dev": true,
+      "requires": {
+        "@babel/helper-explode-as-array": "^7.12.11",
+        "@babel/types": "^7.12.13"
+      }
+    },
+    "@babel/helper-compilation-targets": {
+      "version": "7.13.16",
+      "resolved": "https://registry.npmjs.org/@babel/helper-compilation-targets/-/helper-compilation-targets-7.13.16.tgz",
+      "integrity": "sha512-3gmkYIrpqsLlieJTgb3daTbide1J036OxfptTG/jk+0z1081r6/O+uhyogcfJPVFre04xbbmryo3berRMb0NAA==",
+      "dev": true,
+      "requires": {
+        "@babel/compat-data": "^7.13.15",
+        "@babel/helper-validator-option": "^7.12.17",
+        "browserslist": "^4.14.5",
+        "semver": "^6.3.0"
+      }
+    },
+    "@babel/helper-create-class-features-plugin": {
+      "version": "7.13.11",
+      "resolved": "https://registry.npmjs.org/@babel/helper-create-class-features-plugin/-/helper-create-class-features-plugin-7.13.11.tgz",
+      "integrity": "sha512-ays0I7XYqQRR+Uh2hwV5l
