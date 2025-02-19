0797c3953547fd143035e3e37686dcc6dd54e34b
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 09:49:28 -0500

    Project Auto | Auto modified adblocker/ublock.js

--- adblocker/ublock.js
+++ adblocker/ublock.js
@@ -6,17 +6,69 @@
 - `package.json`
 - `README.md`
 
-## Raw Code for `config.js`
+## Raw Code for `adblocker/ublock.js`
 
 ```javascript
-export const filterLists = [
-  'https://easylist.to/easylist/easylist.txt',
-  'https://easylist.to/easylist/easyprivacy.txt',
-  'https://filterlists.com/subscription.php?address=adblock-plus-subscriptions',
-  'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts'
-];
+importScripts('ublock.worker.js');
 
-export const debug = false;
+const cacheName = 'ublock-cache';
+const cacheAssets = [];
 
-export const proxyPatterns = ['.*'];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(cacheAssets);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        const clonedResponse = response.clone();
+        clonedResponse.headers.append('Cross-Origin-Opener-Policy', 'same-origin');
+        caches.open(cacheName).then((cache) => {
+          cache.put(event.request, clonedResponse);
+        });
+
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data.type === 'blockRequest') {
+    fetch(event.data.url).then((response) => {
+      event.ports[0].postMessage({
+        type: 'blockResponse',
+        url: event.data.url,
+        status: response.status
+      });
+    });
+  }
+});
 ```
