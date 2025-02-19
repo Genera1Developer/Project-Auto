148db14b3f6855ff635584a4bcdddbe81241e65d
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:21:26 -0500

    Project Auto | Repo modified api/search.js

--- api/search.js
+++ api/search.js
@@ -1,1 +1,21 @@
+FILE PATH: api/search.js
+CONTENT: 
+```js
+const axios = require('axios');
+const jsdom = require('jsdom');
 
+async function search(req, res) {
+  try {
+    const { url, query } = req.query;
+    const { data } = await axios.get(url);
+    const { JSDOM } = jsdom;
+    const dom = new JSDOM(data);
+    const results = dom.window.document.querySelectorAll(`a[href^="${query}"]`);
+    res.json({ results });
+  } catch (err) {
+    res.json({ error: 'Error retrieving results' });
+  }
+}
+
+module.exports = search;
+```
