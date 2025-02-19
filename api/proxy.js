422c374957ef78313f273329543e0134122fefe2
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:22:32 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,13 +4,10 @@ content:
 const express = require("express");
 const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const path = require("path");
 
 const app = express();
 
 app.use(cors());
-
-app.use(express.static(path.join(__dirname, "../public")));
 
 const googleProxy = createProxyMiddleware({
   target: "https://google.com",
@@ -30,10 +27,6 @@ const youtubeProxy = createProxyMiddleware({
 
 app.use("/google", googleProxy);
 app.use("/youtube", youtubeProxy);
-
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, '../public/index.html'));
-});
 
 app.listen(3000, () => {
   console.log("Proxy server listening on port 3000");
