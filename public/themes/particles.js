bc2c8c9306e3311a5375827f0ad38cab54cda0fb
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:37:38 -0500

    Project Auto | Repo modified public/themes/particles.js

--- public/themes/particles.js
+++ public/themes/particles.js
@@ -1,20 +1,59 @@
-FILE PATH: public/themes/particles.css
-CONTENT: ```css
-#particles-container {
-  width: 100%;
-  height: 100vh;
-  position: absolute;
-  top: -4px;
-  left: 0;
-  z-index: -2;
-}
+FILE PATH: public/themes/particles.js
+CONTENT: ```javascript
+import Particles from "particles.js";
 
-#particles-js {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  pointer-events: none;
-}
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 3000
+      }
+    },
+    color: {
+      value: "#fff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 1,
+      random: false
+    },
+    size: {
+      value: 2,
+      random: false
+    },
+    line_linked: {
+      enable: false
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: true,
+      straight: false
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble"
+      }
+    },
+    modes: {
+      bubble: {
+        distance: 200,
+        size: 5,
+        duration: 0.4,
+        opacity: 1,
+        speed: 8
+      }
+    }
+  }
+};
+
+export default Particles;
 ```
