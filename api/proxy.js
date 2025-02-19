def4d39497f9d7bced27b7dc089f33012f840a43
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:48:51 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,14 +1,26 @@
-Based on the project goal, provide the file path and content in the following format:
-FILE PATH: index.js
+FILE PATH: api/proxy.js
 CONTENT: 
 ```javascript
+const fetch = require('node-fetch');
 const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+app.get('/', async (req, res) => {
+  const url = req.query.url;
+  if (!url) res.sendStatus(400);
 
-app.listen(3001, () => {
-  console.log('Server is listening on port 3001');
+  const response = await fetch(url);
+  if (!response.ok) res.sendStatus(response.status);
+
+  const contentType = response.headers.get('content-type');
+  res.header('content-type', contentType);
+
+  const body = await response.text();
+  res.send(body);
+});
+
+app.listen(3000, () => {
+  console.log('Proxy server is listening on port 3000');
 });
 ```
