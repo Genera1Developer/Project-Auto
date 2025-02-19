1b0852d3e2cd957ea560b1d9d1e7c659ae086ef4
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:50:31 -0500

    Project Auto | Repo modified api/search.js

--- api/search.js
+++ api/search.js
@@ -4,10 +4,14 @@ FILE PATH: api/search.js
 CONTENT: 
 ```javascript
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
+const API_KEY = 'YOUR_API_KEY';
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+router.get('/', async (req, res) => {
+  const query = req.query.q;
+  const response = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=01234567890123456789:abcdefghijk&q=${query}`);
+  res.json(response.data);
 });
 
 module.exports = router;
