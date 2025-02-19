8c54742478b8d84085bf1e345f68ed326638245e
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:16:35 -0500

    Project Auto | Repo modified api/search.js

--- api/search.js
+++ api/search.js
@@ -1,19 +1,31 @@
-file: api/search.js
+file: public/index.html
 content: 
-```js
-const axios = require('axios');
+```html
+<!DOCTYPE html>
+<html lang="en">
 
-const search = async (req, res) => {
-  try {
-    const { query } = req.body;
-    const response = await axios.get(`https://www.google.com/search?q=${query}`);
-    res.send(response.data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Web Proxy</title>
+  <link rel="stylesheet" href="/styles.css" />
+</head>
 
-module.exports = {
-  search,
-};
+<body>
+  <div id="container">
+    <h1>Web Proxy</h1>
+    <form id="search-form">
+      <input type="text" placeholder="Search..." name="query" />
+      <button type="submit">Go</button>
+    </form>
+
+    <div id="results">
+      <iframe src="" id="iframe"></iframe>
+    </div>
+  </div>
+
+  <script src="/script.js"></script>
+</body>
+
+</html>
 ```
