b119a7b68c3785903d5489d496421070f33cd5a8
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:09:49 -0500

    Project Auto | Repo modified api/search.js

--- api/search.js
+++ api/search.js
@@ -16,18 +16,4 @@ const search = async (req, res) => {
 module.exports = {
   search,
 };
-
-file: index.js
-content: 
-```js
-const express = require('express');
-const apiRouter = require('./api');
-
-const app = express();
-app.use(express.json());
-app.use('/api', apiRouter);
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
 ```
