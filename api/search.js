69b81095066bbdc0c113c2443826f47e4dca955b
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 09:09:20 -0500

    Project Auto | Auto modified api/search.js

--- api/search.js
+++ api/search.js
@@ -1,8 +1,58 @@
-api/search.js can be improved by:
+### Improved Code for `api/search.js`
 
-- Adding a check to ensure the output type is set to 'html' when making the API call, as HTML is required to be correctly parsed.
-- Using a Transform stream to modify the HTML response, rewriting any relative URLs in the response to use `/api/proxy.js` as a proxy and preserve static assets.
-- Flushing the Transform stream to append the closing HTML tags.
+```js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { Transform } = require('stream');
+
+const proxyMiddleware = createProxyMiddleware({
+  target: 'https://example.com',
+  changeOrigin: true,
+});
+
+module.exports = function (req, res) {
+  // Ensure response type is set to HTML
+  req.headers['accept'] = 'text/html';
+
+  // Create a Transform stream to rewrite relative URLs
+  const transformStream = new Transform({
+    transform: (chunk, encoding, next) => {
+      const modifiedChunk = chunk.toString().replace(/="(\/.*?)"/g, '="$1?proxy')
+      next(null, modifiedChunk)
+    },
+    flush: (next) => {
+      next(null, '</body></html>')
+    }
+  })
+
+  // Proxy the request through the middleware
+  proxyMiddleware(req, res, (err) => {
+    if (err) {
+      console.error(err);
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.end('<h1>Internal Server Error</h1>');
+      return;
+    }
+
+    // Pipe the response through the Transform stream
+    res.pipe(transformStream).pipe(res);
+  });
+};
+```
+
+### New File: `api/proxy.js`
+
+This file acts as a proxy for static assets.
+
+```js
+const proxyMiddleware = createProxyMiddleware({
+  target: 'https://example.com',
+  changeOrigin: true,
+});
+
+module.exports = (req, res) => {
+  proxyMiddleware(req, res, (err) => {});
+};
+```
 
 ### File Structure
 
