6b812c5ad555936cde8332af2f0ec9b5505a40a1
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:35:15 -0500

    Project Auto | Repo modified api/search.js

--- api/search.js
+++ api/search.js
@@ -1,14 +1,16 @@
-file: views/layout.pug
+file: controllers/proxy.js
 content: 
-```pug
-doctype html
-html
-  head
-    meta(charset='UTF-8')
-    meta(name='viewport' content='width=device-width, initial-scale=1.0')
-    title Web Proxy
-    link(rel='stylesheet' href='/stylesheets/style.css')
-  body
-    main
-      block content
+```javascript
+const got = require('got');
+
+const proxy = async (req, res) => {
+  const { url } = req.body;
+
+  try {
+    const response = await got(url);
+    res.status(response.statusCode).send(response.body);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 ```
