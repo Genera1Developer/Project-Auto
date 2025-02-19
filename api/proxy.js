11ee5f6b1089840e0a2fe6c4e26862d4796bc290
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:36:03 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,40 +1,14 @@
-FILE PATH: api/utils.js
+FILE PATH: api/proxy.js
 CONTENT: 
 ```javascript
-const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+const utils = require('./utils.js');
 
-const getProxyUrl = (req) => {
-  let url = req.protocol + '://' + req.get('host') + req.originalUrl;
-  return url.replace('api/proxy', 'proxy');
-};
+router.post('/', async (req, res) => {
+  utils.logRequest(req);
+  await utils.forwardRequest(req, res);
+});
 
-const logRequest = (req) => {
-  const url = getProxyUrl(req);
-  const { method, headers, body } = req.body;
-  console.log(`[${method}] ${url}`);
-  console.log('headers:', headers);
-  console.log('body:', body);
-};
-
-const forwardRequest = async (req, res) => {
-  const url = getProxyUrl(req);
-  try {
-    const response = await axios({
-      method: req.body.method,
-      url,
-      headers: req.body.headers,
-      data: req.body.body,
-    });
-
-    res.status(response.status).send(response.data);
-  } catch {
-    res.status(500).send({ error: 'Something went wrong' });
-  }
-};
-
-module.exports = {
-  getProxyUrl,
-  logRequest,
-  forwardRequest,
-};
+module.exports = router;
 ```
