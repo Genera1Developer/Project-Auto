8d76b325b8f606d3dfa4bb60985df414c41ff311
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:14:11 -0500

    Project Auto | Repo modified public/themes/forest.js

--- public/themes/forest.js
+++ public/themes/forest.js
@@ -9,10 +9,77 @@ export default {
         body {
             background: #006400;
             color: #00FF00;
+            font-family: "Helvetica", "Arial", sans-serif;
+            text-align: center;
         }
 
         a {
             color: #FFD700;
+        }
+
+        h1 {
+            margin-bottom: 1.25rem;
+            font-size: 2.5rem;
+        }
+
+        h2 {
+            margin-bottom: 1.25rem;
+            font-size: 2rem;
+        }
+
+        h3 {
+            margin-bottom: 1.25rem;
+            font-size: 1.75rem;
+        }
+
+        h4 {
+            margin-bottom: 1.25rem;
+            font-size: 1.5rem;
+        }
+
+        h5 {
+            margin-bottom: 1.25rem;
+            font-size: 1.25rem;
+        }
+
+        h6 {
+            margin-bottom: 1.25rem;
+            font-size: 1.125rem;
+        }
+
+        p {
+            margin-bottom: 1.25rem;
+            font-size: 1rem;
+        }
+
+        ul {
+            list-style-type: none;
+            display: flex;
+            padding: 0;
+        }
+
+        li {
+            margin-right: 1.25rem;
+            font-size: 1.25rem;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 1.25rem;
+        }
+
+        .header {
+            margin-bottom: 1.25rem;
+        }
+
+        .footer {
+            margin-top: 1.25rem;
+            text-align: right;
         }
     `
 };
