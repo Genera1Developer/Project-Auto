e4319cd0aa86679b3e74840461088f85320d01ca
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 16:38:00 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,52 +1,19 @@
-FILE PATH: api/utils.js
+FILE PATH: api/proxy.js
 CONTENT: 
 ```javascript
-const fetch = require('node-fetch');
-const fs = require('fs');
-const crypto = require('crypto');
-const path = require('path');
+const express = require('express');
+const app = express();
+const { logRequest, forwardRequest } = require('./utils');
+const bodyParser = require('body-parser');
 
-const getTempFilePath = async (req) => {
-  const filename = crypto.createHash('md5').update(req.url).digest('hex');
-  const tempDir = path.join(__dirname, 'temp');
-  const filePath = path.join(tempDir, filename);
-  if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir);
-  }
-  return filePath;
-};
+app.use(bodyParser.json());
 
-const saveRequest = async (req, filePath) => {
-  const request = {
-    url: req.url,
-    method: req.method,
-    headers: req.headers,
-    body: req.body
-  };
-  const json = JSON.stringify(request);
-  await fs.promises.writeFile(filePath, json);
-};
+app.post('/', async (req, res) => {
+  await logRequest(req);
+  await forwardRequest(req, res);
+});
 
-const logRequest = async (req) => {
-  const filePath = await getTempFilePath(req);
-  await saveRequest(req, filePath);
-};
-
-const forwardRequest = async (req, res) => {
-  const url = req.body.url;
-  const options = {
-    headers: req.headers,
-    method: req.method,
-    body: req.body,
-  };
-  const response = await fetch(url, options);
-  res.status(response.status);
-  response.headers.forEach((val, key) => res.header(key, val));
-  response.body.pipe(res);
-};
-
-module.exports = {
-  logRequest,
-  forwardRequest,
-};
+app.listen(3000, () => {
+  console.log('Proxy server listening on port 3000');
+});
 ```
