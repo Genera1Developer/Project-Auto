378ebffd88c30b76b5516381c1d4b79bee26ae56
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:12:21 -0500

    Project Auto | Repo modified api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,29 +1,21 @@
 file path: api/proxy.js
 content: 
 ```javascript
-const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
 app.use(cors());
 
-app.get("/proxy", async (req, res) => {
-  const url = req.query.url;
-
-  if (!url) {
-    return res.status(400).send("Missing URL parameter");
-  }
-
-  try {
-    const response = await axios.get(url);
-    const data = response.data;
-    res.json(data);
-  } catch (error) {
-    res.status(500).send("Internal server error");
-  }
-});
+app.use(
+  "/proxy",
+  createProxyMiddleware({
+    target: "https://example.com",
+    changeOrigin: true,
+  })
+);
 
 app.listen(3000, () => {
   console.log("Proxy server listening on port 3000");
