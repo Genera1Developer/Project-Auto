ac97113c6ba445e2c701ef0faf2921f9aeba3f4a
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-19 17:12:47 -0500

    Project Auto | Repo modified public/themes/forest.js

--- public/themes/forest.js
+++ public/themes/forest.js
@@ -1,9 +1,19 @@
 FILE PATH: public/themes/forest.js
 CONTENT: 
 ```javascript
+import { css } from "../../../lib/theme.js";
 
 export default {
     name: 'Forest',
-    css: './forest.css'
+    css: css`
+        body {
+            background: #006400;
+            color: #00FF00;
+        }
+
+        a {
+            color: #FFD700;
+        }
+    `
 };
 ```
