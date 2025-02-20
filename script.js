5f7e2ca4e29cce1665a8adccb1b6e3a1596e6177
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-20 17:38:35 -0500

    Project Auto | Repo modified script.js

--- script.js
+++ script.js
@@ -27,6 +27,23 @@
         </svg>
       </button>
     </div>
+    <div class="light-dark-mode-toggle">
+      <input type="checkbox" id="light-dark-mode-toggle-input" />
+      <label for="light-dark-mode-toggle-input" class="light-dark-mode-toggle-label">
+        <div class="sun">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFE169">
+            <path
+              d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8z" />
+          </svg>
+        </div>
+        <div class="moon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#2E3133">
+            <path
+              d="M20 12a10.54 10.54 0 0 0-7.51-3.51A1.19 1.19 0 0 0 12 9.46V4h2v5.46a1.19 1.19 0 0 0 2.38.03A10.54 10.54 0 0 0 20 12zm0 0a9.46 9.46 0 0 1-6.5-2.51A1.19 1.19 0 0 0 12 3.46V0h2v3.46a1.19 1.19 0 0 0 2.38.03A9.46 9.46 0 0 1 20 12z" />
+          </svg>
+        </div>
+      </label>
+    </div>
     <form id="login-form">
       <label for="username">Username:</label>
       <input type="text" id="username" />
