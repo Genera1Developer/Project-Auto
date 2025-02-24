e2f616676792ec63fdd388636f513b78993ed859
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-24 16:00:47 -0500

    const view = (fn) => new Proxy(fn, {

--- proxy.js
+++ proxy.js
@@ -1,7 +1,1 @@
-const view = (fn) => new Proxy(fn, {
-  get: (target, prop, _receiver) => prop === 'name' ? fn.name : typeof target[prop] === 'function' ? view(target[prop]) : target[prop],
-  apply: (target, _, args) => {
-    if (target.name) console.log(`Function ${target.name} called with args: ${args}`);
-    return target(...args);
-  },
-});
+---START---
