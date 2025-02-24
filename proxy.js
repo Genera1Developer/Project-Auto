beda920445d8d8f0e87c534763081b80140ddb31
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-24 15:55:52 -0500

    Updated proxy.js

--- proxy.js
+++ proxy.js
@@ -1,5 +1,5 @@
 const view = (fn) => new Proxy(fn, {
-  get: (target, prop) => (typeof target[prop] === 'function' ? view(target[prop]) : target[prop]),
+  get: (target, prop) => (prop === 'name' ? fn.name : (typeof target[prop] === 'function' ? view(target[prop]) : target[prop])),
   apply: (target, _, args) => {
     if (target.name) console.log(`Function ${target.name} called with args: ${args}`);
     return target(...args);
