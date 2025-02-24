894f8590d8789940f3afc971878f67d994cca63b
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-24 15:44:39 -0500

    Updated proxy.js

--- proxy.js
+++ proxy.js
@@ -1,1 +1,8 @@
-
+const viewProxy = (fn) => {
+  return new Proxy(fn, {
+    apply: (target, thisArg, argumentsList) => {
+      console.log(`${target.name} was called with args: ${argumentsList}`);
+      return target.apply(thisArg, argumentsList);
+    },
+  });
+};
