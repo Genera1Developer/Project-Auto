8805a60fb6acff35a7b4ffa2889e59c0b15e3f24
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-24 16:36:15 -0500

    fix(scripts): change the default name to Alice

--- settings.js
+++ settings.js
@@ -1,1 +1,1 @@
-'// Settings here are used to build the Chat Platform\nmodule.exports = {chatClient: {helloWorld: "Girish"}};'
+'// Settings here are used to build the Chat Platform\nmodule.exports = {chatClient: {helloWorld: "Alice"}};'
