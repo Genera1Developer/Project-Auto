6ad140d543cb658d9d087e9924f2c16d862c36a8
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-24 16:47:59 -0500

    Improve proxy.js and organize sampleObj keys

--- proxy.js
+++ proxy.js
@@ -1,1 +1,1 @@
-"module.exports = { foo: \"bar\", baz: \"qux\", sampleJson: {\"sample\": 1, \"sampleString\": \"string\"}, sampleArr: [1, \"string\"], sampleFunc: (arg) => { console.log(\"Sample function called!\", arg); return arg; }, sampleObj: { sub_sample: 4 }, moduleVar: \"improved new\" };"
+"""module.exports = { foo: "bar", baz: "qux", sampleJson: {"sample": 1, "sampleString": "string"}, sampleArr: [1, "string"], sampleFunc: (arg) => { console.log("Sample function called!", arg); return arg; }, sampleObj: { sub_sample: "improved" }, moduleVar: "improved new" };"""
