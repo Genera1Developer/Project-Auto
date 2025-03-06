63f98bf8fa7d8cbc86f19877d7d14e34e3976fd0
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 15:36:51 -0500

    Delete api directory

--- api/cache.js
+++
@@ -1,8 +0,0 @@
-const NodeCache = require( "node-cache" );
-
-const stdTTL = 60 * 60 * 24; // 24 hours
-const checkperiod = 60 * 60; // 1 hour
-
-const cache = new NodeCache( { stdTTL: stdTTL, checkperiod: checkperiod } );
-
-module.exports = cache;
--- api/errorHandling.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error('Global error handler:', err);
-    res.status(500).json({ error: 'Internal Server Error', message: err.message });
-};
-
-module.exports = errorHandler;
