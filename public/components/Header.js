742a760db3da1efbc2f1c48200be0db968a90a40
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 17:19:01 -0500

    Added Header component edit filepath: public/co...

---
+++ public/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return `
+        <header>
+            <h1>Web Proxy</h1>
+            <p>Enter the URL you want to access:</p>
+        </header>
+    `;
+}
+
+export default Header;
