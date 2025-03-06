cd3845724d69199bb5a0ba6ebefde59b5bc23aec
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 16:59:42 -0500

    Implement caching mechanism for proxy responses...

---
+++ api/cache.js
@@ -0,0 +1,18 @@
+const NodeCache = require( "node-cache" );
+
+const cache = new NodeCache({ stdTTL: 300, checkperiod: 120 });
+
+module.exports = {
+  get: (key) => {
+    return cache.get(key);
+  },
+  set: (key, value) => {
+    cache.set(key, value);
+  },
+  del: (key) => {
+    cache.del(key);
+  },
+  flush: () => {
+    cache.flushAll();
+  }
+};
