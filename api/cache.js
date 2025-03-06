4108e2a44dfde7c9789177685bf2416c518fd9d7
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 17:18:31 -0500

    Refactor cache implementation to use node-cache...

--- api/cache.js
+++ api/cache.js
@@ -1,13 +1,18 @@
-const cache = {};
+const NodeCache = require( 'node-cache' );
+
+const cache = new NodeCache({ stdTTL: 3600, checkperiod: 600 });
 
 module.exports = {
     get: (url) => {
-        return cache[url] || null;
+        return cache.get(url) || null;
     },
     set: (url, data) => {
-        cache[url] = data;
+        cache.set(url, data);
     },
     clear: (url) => {
-        delete cache[url];
+        cache.del(url);
+    },
+    flush: () => {
+        cache.flushAll();
     }
 };
