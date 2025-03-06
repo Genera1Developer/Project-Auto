53ce7105cfc73723872db04281ad801c3c5001d0
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 17:56:31 -0500

    Added input validation to hashString and added ...

--- api/security.js
+++ api/security.js
@@ -5,12 +5,21 @@ function generateRandomToken(length = 32) {
 }
 
 function hashString(string, salt) {
+  if (!string || !salt) {
+    throw new Error('String and salt are required for hashing.');
+  }
   const hash = crypto.createHmac('sha512', salt);
   hash.update(string);
   return hash.digest('hex');
 }
 
+function verifyHash(string, hash, salt) {
+  const computedHash = hashString(string, salt);
+  return computedHash === hash;
+}
+
 module.exports = {
   generateRandomToken,
   hashString,
+  verifyHash,
 };
