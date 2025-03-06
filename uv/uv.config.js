8e2462487227d9b587fca318d741efb5e25ece4a
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 18:45:33 -0500

    Add config file for Ultraviolet edit filepath: ...

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,15 @@
-export default {
-  prefix: '/service/',
-  bare: '/bare/',
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: '/uv/uv.handler.js',
-  bundle: '/uv/uv.bundle.js',
-  config: '/uv/uv.config.js',
-  sw: '/uv/uv.sw.js',
-};
+(() => {
+  "use strict";
+  const config = {
+    prefix: '/service/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+  };
+
+  self.uv = config;
+})();
