fee31030df64fb9e921f9dfd99d927c41084afc7
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 17:08:33 -0500

    Implement basic caching mechanism edit filepath...

--- api/cache.js
+++ api/cache.js
@@ -1,18 +1,10 @@
-const NodeCache = require( "node-cache" );
-
-const cache = new NodeCache({ stdTTL: 300, checkperiod: 120 }); // TTL: 5 minutes, check every 2 minutes
+const cache = {};
 
 module.exports = {
-  get: (key) => {
-    return cache.get(key);
-  },
-  set: (key, value) => {
-    cache.set(key, value);
-  },
-  del: (key) => {
-    cache.del(key);
-  },
-  flush: () => {
-    cache.flushAll();
-  }
+    get: (url) => {
+        return cache[url] || null;
+    },
+    set: (url, data) => {
+        cache[url] = data;
+    }
 };
