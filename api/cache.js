67d4794e77073deefcedaa10a2f96ba611b848bd
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 17:02:37 -0500

    Implement basic in-memory caching edit filepath...

--- api/cache.js
+++ api/cache.js
@@ -1,35 +1,10 @@
-const NodeCache = require( "node-cache" );
-
-const cache = new NodeCache({ stdTTL: 300, checkperiod: 120 });
+const cache = {};
 
 module.exports = {
-  get: (key) => {
-    try {
-      return cache.get(key);
-    } catch (err) {
-      console.error(`Cache get error for key ${key}: ${err}`);
-      return undefined;
+    get: (url) => {
+        return cache[url] || null;
+    },
+    set: (url, data) => {
+        cache[url] = data;
     }
-  },
-  set: (key, value) => {
-    try {
-      cache.set(key, value);
-    } catch (err) {
-      console.error(`Cache set error for key ${key}: ${err}`);
-    }
-  },
-  del: (key) => {
-    try{
-      cache.del(key);
-    } catch (err) {
-      console.error(`Cache delete error for key ${key}: ${err}`);
-    }
-  },
-  flush: () => {
-    try {
-      cache.flushAll();
-    } catch (err) {
-      console.error(`Cache flush error: ${err}`);
-    }
-  }
 };
