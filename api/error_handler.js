61cb44d8b6afc2cccec70e954bbd2c666eae7778
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 18:06:45 -0500

    Improved error handling with specific checks fo...

--- api/error_handler.js
+++ api/error_handler.js
@@ -1,11 +1,20 @@
 function errorHandler(err, req, res, next) {
-  console.error(err.stack);
+  console.error(err);
 
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message || 'Internal Server Error';
+
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+    message = err.message;
+  } else if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    statusCode = 404;
+    message = 'Resource not found';
+  }
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
   });
 }
 
