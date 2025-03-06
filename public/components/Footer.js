bff25c658ae77aa7ef4a33f9da985c12f75c550a
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 17:52:36 -0500

    Added links to Privacy Policy and Terms of Serv...

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
         <footer className="bg-light py-3 mt-auto">
             <div className="container">
                 <p className="text-center">
-                    &copy; {new Date().getFullYear()} MyProxy. All rights reserved.
+                    &copy; {new Date().getFullYear()} MyProxy - All rights reserved. | <a href="/privacy">Privacy Policy</a> | <a href="/terms">Terms of Service</a>
                 </p>
             </div>
         </footer>
