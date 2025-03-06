3aae4f0beea6dce187aaa9a10cc0fb361c56002e
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 18:11:20 -0500

    Improved proxy handler by removing hop-by-hop h...

--- api/proxy_handler.js
+++ api/proxy_handler.js
@@ -14,24 +14,41 @@ const proxyHandler = (req, res) => {
     const options = {
       method: req.method,
       headers: { ...req.headers, host: parsedURL.hostname },
-      followRedirects: false, // Prevent proxy from following redirects
+      followRedirects: false,
     };
 
+    // Remove hop-by-hop headers
+    delete options.headers['connection'];
+    delete options.headers['proxy-connection'];
+    delete options.headers['transfer-encoding'];
+    delete options.headers['upgrade'];
+
     const proxyReq = (parsedURL.protocol === 'https:' ? https : http).request(parsedURL.href, options, (proxyRes) => {
+      // Remove hop-by-hop headers from the response as well
+      delete proxyRes.headers['connection'];
+      delete proxyRes.headers['proxy-connection'];
+      delete proxyRes.headers['transfer-encoding'];
+      delete proxyRes.headers['upgrade'];
+
       res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
+      proxyRes.pipe(res);
     });
 
     proxyReq.on('error', (err) => {
       console.error('Proxy request error:', err);
-      return res.status(500).send('Proxy error.');
+      if (!res.headersSent) {
+        return res.status(500).send('Proxy error.');
+      } else {
+        // If headers already sent, close the connection
+        res.end();
+      }
     });
 
-    req.pipe(proxyReq, { end: true });
+    req.pipe(proxyReq);
 
     req.on('error', (err) => {
       console.error('Request pipe error:', err);
-      proxyReq.destroy(err); // Destroy the proxy request to prevent hanging
+      proxyReq.destroy(err);
     });
 
     proxyReq.on('close', () => {
