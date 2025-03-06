a3803934932ac7aa58c4382b52b6cb31f42d3943
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 07:29:05 -0500

    Add protocol check to isValidURL function for enhanced validation.

--- utils/urlUtils.js
+++ utils/urlUtils.js
@@ -1,5 +1,9 @@
 function isValidURL(url) {
     try {
+        // Check if the URL starts with a valid protocol (http/https)
+        if (!url.startsWith('http://') && !url.startsWith('https://')) {
+            return false;
+        }
         new URL(url);
         return true;
     } catch (_) {
