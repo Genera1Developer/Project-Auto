8924a49c33a472c9e71ad66368fd81fa920a74f7
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 18:21:38 -0500

    Add configuration file for Ultraviolet edit fil...

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,21 +1,15 @@
 (() => {
   "use strict";
-  const uv = {
-    prefix: "/service/",
-    bare: "/bare/",
+  const config = {
+    prefix: '/service/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
   };
-
-  // Check if running in a service worker context
-  if (typeof self !== 'undefined' && 'ServiceWorkerGlobalScope' in self) {
-    self.uv = uv;
-  } else {
-    // Expose uv for non-service worker contexts (e.g., window)
-    window.uv = uv;
-  }
+  self.Ultraviolet = self.Ultraviolet || {};
+  self.Ultraviolet.config = config;
 })();
