71212bc5af4dec1c9bcf7cab3920ccb490339e4a
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 18:17:56 -0500

    Added timeout and keep-alive header removal for...

--- api/proxy_handler.js
+++ api/proxy_handler.js
@@ -15,6 +15,7 @@ const proxyHandler = (req, res) => {
       method: req.method,
       headers: { ...req.headers, host: parsedURL.hostname },
       followRedirects: false,
+      timeout: 15000, // Add a timeout to prevent hanging requests
     };
 
     // Remove hop-by-hop headers
@@ -22,6 +23,7 @@ const proxyHandler = (req, res) => {
     delete options.headers['proxy-connection'];
     delete options.headers['transfer-encoding'];
     delete options.headers['upgrade'];
+    delete options.headers['keep-alive']; // Also remove keep-alive
 
     const proxyReq = (parsedURL.protocol === 'https:' ? https : http).request(parsedURL.href, options, (proxyRes) => {
       // Remove hop-by-hop headers from the response as well
@@ -29,9 +31,20 @@ const proxyHandler = (req, res) => {
       delete proxyRes.headers['proxy-connection'];
       delete proxyRes.headers['transfer-encoding'];
       delete proxyRes.headers['upgrade'];
+      delete proxyRes.headers['keep-alive']; // Also remove keep-alive
 
       res.writeHead(proxyRes.statusCode, proxyRes.headers);
       proxyRes.pipe(res);
+    });
+
+    proxyReq.on('timeout', () => {
+      console.error('Proxy request timed out.');
+      proxyReq.destroy(); // Destroy the request
+      if (!res.headersSent) {
+        return res.status(504).send('Proxy request timed out.');
+      } else {
+        res.end();
+      }
     });
 
     proxyReq.on('error', (err) => {
