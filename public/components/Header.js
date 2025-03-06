7cc034c96d01262734cdd1755ac825ba317e1e3d
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 17:21:09 -0500

    Added a div with class 'header-content' for pot...

--- public/components/Header.js
+++ public/components/Header.js
@@ -1,8 +1,10 @@
 function Header() {
     return `
         <header>
-            <h1>Web Proxy</h1>
-            <p>Enter the URL you want to access:</p>
+            <div class="header-content">
+                <h1>Web Proxy</h1>
+                <p>Enter the URL you want to access:</p>
+            </div>
         </header>
     `;
 }
