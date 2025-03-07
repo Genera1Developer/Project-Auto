f7a103b487c54620a77cd73638047629e5194cf4
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 20:13:57 -0500

    Refactor: Removed inline styles and added class...

--- public/components/Header.js
+++ public/components/Header.js
@@ -1,8 +1,8 @@
 const Header = () => {
     return `
-        <header style="text-align: center; padding: 20px; background-color: #f0f0f0;">
-            <h1 style="color: #333;">Web Proxy</h1>
-            <p style="color: #666;">Enter a URL to browse anonymously.</p>
+        <header class="header">
+            <h1>Web Proxy</h1>
+            <p>Enter a URL to browse anonymously.</p>
         </header>
     `;
 }
