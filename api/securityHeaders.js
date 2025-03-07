43a460947e7b130fa6ade686c972416cc5c6a194
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 18:58:00 -0500

    Add security headers middleware edit filepath: ...

---
+++ api/securityHeaders.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self';");
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.setHeader('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
+  next();
+};
