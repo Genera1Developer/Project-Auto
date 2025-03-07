f453feb99d80c527a13725a1458286ad632ee051
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 23:04:58 -0500

    Added basic AES encryption/decryption module ed...

--- api/security.js
+++ api/security.js
@@ -5,19 +5,19 @@ const key = crypto.randomBytes(32); // 256 bits
 const iv = crypto.randomBytes(16); // Initialization vector
 
 function encrypt(text) {
-    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
 }
 
-function decrypt(text, ivHex) {
-    const iv = Buffer.from(ivHex, 'hex');
-    const encryptedText = Buffer.from(text, 'hex');
-    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
+function decrypt(text) {
+  const iv = Buffer.from(text.iv, 'hex');
+  const encryptedText = Buffer.from(text.encryptedData, 'hex');
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 }
 
 module.exports = { encrypt, decrypt };
