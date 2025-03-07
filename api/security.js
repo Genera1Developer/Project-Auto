149021bccadb2dccdbb2cbcfb4018ccc93c9708a
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 23:31:24 -0500

    Added AES encryption functions edit filepath: a...

--- api/security.js
+++ api/security.js
@@ -1,23 +1,26 @@
 const crypto = require('crypto');
 
 const algorithm = 'aes-256-cbc';
-const key = crypto.randomBytes(32); // Generate a secure random key
-const iv = crypto.randomBytes(16); // Generate a secure random IV
+const key = crypto.randomBytes(32); // Generate a secure key
+const iv = crypto.randomBytes(16); // Generate a secure IV
 
 function encrypt(text) {
-    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
     let encrypted = cipher.update(text);
+
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
+
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
 }
 
-function decrypt(text) {
-    const textParts = text.split(':');
-    const iv = Buffer.from(textParts.shift(), 'hex');
-    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+function decrypt(text, ivHex) {
+    let iv = Buffer.from(ivHex, 'hex');
+    let encryptedText = Buffer.from(text, 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
     let decrypted = decipher.update(encryptedText);
+
     decrypted = Buffer.concat([decrypted, decipher.final()]);
+
     return decrypted.toString();
 }
 
