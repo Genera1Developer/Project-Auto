3ee014aa7691bca12ac3fe99701480f2e2445675
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 20:25:36 -0500

    Added try-catch to encode function, removed unn...

--- uv/codec.js
+++ uv/codec.js
@@ -1,10 +1,15 @@
 export function encode(str) {
-  return btoa(String.fromCharCode(...new TextEncoder().encode(str)));
+  try {
+    return btoa(String.fromCharCode(...new TextEncoder().encode(str)));
+  } catch (e) {
+    console.error("Encoding error:", e);
+    return null;
+  }
 }
 
 export function decode(str) {
   try {
-    return new TextDecoder().decode(Uint8Array.from(atob(str), c => c.charCodeAt(0)));
+    return new TextDecoder().decode(Uint8Array.from(atob(str, ), c => c.charCodeAt(0)));
   } catch (e) {
     console.error("Decoding error:", e);
     return null;
