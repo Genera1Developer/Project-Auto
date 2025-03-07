8853e710eda8210a3c3ea899232e76282db76242
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 18:00:59 -0500

    Add security headers edit filepath: uv/codec.js...

--- api/security.js
+++ api/security.js
@@ -4,6 +4,4 @@ export function addSecurityHeaders(res) {
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-Frame-Options', 'DENY');
     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-    res.setHeader('Feature-Policy', "microphone 'none'; geolocation 'none'; camera 'none'");
-    res.setHeader('Permissions-Policy', "microphone=(), geolocation=(), camera=()");
 }
