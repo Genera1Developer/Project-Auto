934d305eb57859fb03b802f633f7aac7d6bc55fd
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 17:22:17 -0500

    Add security utils: key generation, encryption,...

--- api/security.js
+++ api/security.js
@@ -1,52 +1,29 @@
 const crypto = require('crypto');
 
-const generateSecureKey = (length = 32) => {
+function generateRandomKey(length = 32) {
   return crypto.randomBytes(length).toString('hex');
-};
+}
 
-const generateSecureSessionId = () => {
-  return crypto.randomBytes(24).toString('hex');
-};
-
-const hashPassword = (password) => {
-  const salt = crypto.randomBytes(16).toString('hex'); // Generate a unique salt for each password
-  const hash = crypto.createHmac('sha512', salt);
-  hash.update(password);
-  return {
-    salt: salt,
-    hash: hash.digest('hex')
-  };
-};
-
-const verifyPassword = (password, hashedPassword, salt) => {
-  const verificationHash = crypto.createHmac('sha512', salt);
-  verificationHash.update(password);
-  const calculatedHash = verificationHash.digest('hex');
-  return calculatedHash === hashedPassword;
-};
-
-const encryptData = (data, encryptionKey) => {
+function encrypt(text, key) {
   const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(encryptionKey, 'hex'), iv);
-  let encrypted = cipher.update(JSON.stringify(data), 'utf8', 'hex'); // Specify input encoding
-  encrypted += cipher.final('hex'); // Specify output encoding
-  return { iv: iv.toString('hex'), encryptedData: encrypted }; //Return directly the hex encoded string
-};
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
 
-const decryptData = (encryptedData, encryptionKey, iv) => {
-  const ivBuffer = Buffer.from(iv, 'hex');
-  const encryptedText = encryptedData //No conversion needed as encryptData returns a string directly; Buffer.from(encryptedData, 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey, 'hex'), ivBuffer);
-  let decrypted = decipher.update(encryptedText, 'hex', 'utf8'); //Specify encoding
-  decrypted += decipher.final('utf8'); //Specify encoding
-  return JSON.parse(decrypted);
-};
+function decrypt(text, key) {
+  const textParts = text.split(':');
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
 
 module.exports = {
-  generateSecureKey,
-  generateSecureSessionId,
-  hashPassword,
-  verifyPassword,
-  encryptData,
-  decryptData
+  generateRandomKey,
+  encrypt,
+  decrypt
 };
