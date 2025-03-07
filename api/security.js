6f55d06e46565f086391005a4520ae2b86135d59
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 07:34:53 -0500

    Implement AES encryption and decryption functio...

--- api/security.js
+++ api/security.js
@@ -8,16 +8,20 @@ function encrypt(text) {
     const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
 function decrypt(text) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    const textParts = text.split(':');
+    const iv = Buffer.from(textParts.shift(), 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
     const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
 }
 
-module.exports = { encrypt, decrypt };
+module.exports = {
+    encrypt,
+    decrypt
+};
