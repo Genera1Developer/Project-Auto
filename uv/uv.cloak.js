3081b80caef0da309b0e54ebafd7664e29636bcf
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 20:34:57 -0500

    Improved proxy handler with Reflect and added c...

--- uv/uv.cloak.js
+++ uv/uv.cloak.js
@@ -1,21 +1,27 @@
-(() => {
-  const handler = {
+const handler = {
     get: function(target, prop) {
-      if (prop === 'toString') {
-        return () => '[object Object]';
-      }
-      if (prop === Symbol.toStringTag) {
-        return 'Object';
-      }
-      try {
-        return target[prop];
-      } catch (e) {
-        return undefined;
-      }
+        if (prop === 'toString') {
+            return () => '[object Object]';
+        }
+        if (prop === Symbol.toStringTag) {
+            return 'Object';
+        }
+        try {
+            return Reflect.get(target, prop);
+        } catch (e) {
+            return undefined;
+        }
+    },
+    set: function(target, prop, value) {
+        try {
+            return Reflect.set(target, prop, value);
+        } catch (e) {
+            return false;
+        }
     },
     apply: function(target, thisArg, argumentsList) {
         try {
-            return target.apply(thisArg, argumentsList);
+            return Reflect.apply(target, thisArg, argumentsList);
         } catch (e) {
             return undefined;
         }
@@ -27,20 +33,22 @@
             return undefined;
         }
     }
-  };
+};
 
-  window.XMLHttpRequest = new Proxy(window.XMLHttpRequest, handler);
-  window.WebSocket = new Proxy(window.WebSocket, handler);
-
-  if (window.fetch) {
-    window.fetch = new Proxy(window.fetch, handler);
-  }
-
-  if (window.navigator) {
-    window.navigator = new Proxy(window.navigator, handler);
-  }
-
-  if (window.document) {
-    window.document = new Proxy(window.document, handler);
-  }
+(() => {
+    if (window.XMLHttpRequest) {
+        window.XMLHttpRequest = new Proxy(window.XMLHttpRequest, handler);
+    }
+    if (window.WebSocket) {
+        window.WebSocket = new Proxy(window.WebSocket, handler);
+    }
+    if (window.fetch) {
+        window.fetch = new Proxy(window.fetch, handler);
+    }
+    if (window.navigator) {
+        window.navigator = new Proxy(window.navigator, handler);
+    }
+    if (window.document) {
+        window.document = new Proxy(window.document, handler);
+    }
 })();
