5fa7fbafd8f4fb27a5b089325e9e11a243e8b973
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 20:36:32 -0500

    Improved encode and decode functions for better...

--- uv/codec.js
+++ uv/codec.js
@@ -1,6 +1,6 @@
 export function encode(str) {
   try {
-    return btoa(String.fromCharCode(...new TextEncoder().encode(str)));
+    return btoa(String.fromCharCode.apply(null, new TextEncoder().encode(str)));
   } catch (e) {
     console.error("Encoding error:", e);
     return null;
@@ -9,7 +9,7 @@ export function encode(str) {
 
 export function decode(str) {
   try {
-    return new TextDecoder().decode(Uint8Array.from(atob(str, ), c => c.charCodeAt(0)));
+    return new TextDecoder().decode(new Uint8Array(Array.from(atob(str), c => c.charCodeAt(0))));
   } catch (e) {
     console.error("Decoding error:", e);
     return null;
