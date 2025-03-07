4eba8cca2e6c87770e0043210c4e9f4aaa9f6773
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 18:27:48 -0500

    Improved encryption to AES-256-GCM for authenti...

--- api/security.js
+++ api/security.js
@@ -5,21 +5,24 @@ function generateSecureKey(length = 32) {
   return crypto.randomBytes(length).toString('hex');
 }
 
-// Function to encrypt data using AES
+// Function to encrypt data using AES-GCM
 function encrypt(data, key) {
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv);
+  const iv = crypto.randomBytes(12);
+  const cipher = crypto.createCipheriv('aes-256-gcm', Buffer.from(key, 'hex'), iv);
   let encrypted = cipher.update(data);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
-  return iv.toString('hex') + ':' + encrypted.toString('hex');
+  const authTag = cipher.getAuthTag();
+  return iv.toString('hex') + ':' + authTag.toString('hex') + ':' + encrypted.toString('hex');
 }
 
-// Function to decrypt data using AES
+// Function to decrypt data using AES-GCM
 function decrypt(data, key) {
   const textParts = data.split(':');
   const iv = Buffer.from(textParts.shift(), 'hex');
+  const authTag = Buffer.from(textParts.shift(), 'hex');
   const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv);
+  const decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(key, 'hex'), iv);
+  decipher.setAuthTag(authTag);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
