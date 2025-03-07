f5480cd6ef99c1123ccdaeeed06cb084cf2f6e45
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 07:18:38 -0500

    Implement caching of fetched resources and cach...

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,44 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+
+      const fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        (response) => {
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+
+          caches.open('my-cache')
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        }
+      );
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['my-cache'];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
