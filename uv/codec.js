181c28bee172cb01d6cf28c7f5e6418dea17fb00
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 20:05:53 -0500

    Use TextEncoder/Decoder for more robust UTF-8 h...

--- uv/codec.js
+++ uv/codec.js
@@ -1,10 +1,10 @@
 export function encode(str) {
-  return btoa(unescape(encodeURIComponent(str)));
+  return btoa(String.fromCharCode(...new TextEncoder().encode(str)));
 }
 
 export function decode(str) {
   try {
-    return decodeURIComponent(escape(atob(str)));
+    return new TextDecoder().decode(Uint8Array.from(atob(str), c => c.charCodeAt(0)));
   } catch (e) {
     console.error("Decoding error:", e);
     return null;
