99635bef1edada6fbb3ddeada9be8f4c6778a55e
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 19:56:15 -0500

    Cloak XMLHttpRequest, WebSocket, fetch, navigat...

--- uv/uv.cloak.js
+++ uv/uv.cloak.js
@@ -1,1 +1,28 @@
+(() => {
+  const handler = {
+    get: function(target, prop) {
+      if (prop === 'toString') {
+        return () => '[object Object]';
+      }
+      if (prop === Symbol.toStringTag) {
+        return 'Object';
+      }
+      return target[prop];
+    }
+  };
 
+  window.XMLHttpRequest = new Proxy(window.XMLHttpRequest, handler);
+  window.WebSocket = new Proxy(window.WebSocket, handler);
+
+  if (window.fetch) {
+    window.fetch = new Proxy(window.fetch, handler);
+  }
+
+  if (window.navigator) {
+    window.navigator = new Proxy(window.navigator, handler);
+  }
+
+  if (window.document) {
+    window.document = new Proxy(window.document, handler);
+  }
+})();
