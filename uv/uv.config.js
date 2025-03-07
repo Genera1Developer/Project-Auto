6024ed1f19501a95de52a37232d9dfb8294615e2
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 07:16:06 -0500

    Switched to base64 encoding and added a placeho...

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,13 @@
 self.__uv$config = {
     prefix: '/service/',
     bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
+    encodeUrl: Ultraviolet.codec.base64.encode,
+    decodeUrl: Ultraviolet.codec.base64.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
     injectedScripts: '/uv/uv.injected.js',
-    xorKey: 'AAAAAAAAAAAAAAAA',
+    // Increase key length for better security
+    xorKey: 'randomlyGeneratedSecureKey',
 };
