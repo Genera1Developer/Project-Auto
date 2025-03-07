56571a340c397ef56a6df7eb0ae2eecc3c683d63
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-06 20:17:11 -0500

    Added a "Contact Us" link to the footer.

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -4,10 +4,12 @@ function Footer() {
             <div className="container">
                 <p className="text-center">
                     &copy; {new Date().getFullYear()} MyProxy - All rights reserved.
-                    <span className="mx-2">|</span>
+                    <span className="mx-2">&#124;</span>
                     <a href="/privacy" className="text-decoration-none">Privacy Policy</a>
-                    <span className="mx-2">|</span>
+                    <span className="mx-2">&#124;</span>
                     <a href="/terms" className="text-decoration-none">Terms of Service</a>
+                    <span className="mx-2">&#124;</span>
+                    <a href="/contact" className="text-decoration-none">Contact Us</a>
                 </p>
             </div>
         </footer>
