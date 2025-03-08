07f60463a4bfd63f8ab8cc7ba7d96ea3ef8ec002
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 22:39:21 -0500

    Use AES for URL encoding/decoding, added AES ke...

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,11 @@
 self.__uv$config = {
     prefix: '/service/',
     bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
+    encodeUrl: Ultraviolet.codec.aes.encode,
+    decodeUrl: Ultraviolet.codec.aes.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
+    aesKey: 'ENCRYPTION_KEY_HERE' // Replace with a strong, randomly generated key
 };
