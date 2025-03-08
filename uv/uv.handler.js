8f815d50f7a8b7b5eba502fc639639847de9fe95
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 22:41:53 -0500

    Update UV config with more bare servers edit fi...

--- uv/uv.handler.js
+++ uv/uv.handler.js
@@ -1,1 +1,16 @@
-
+self.__uv$config = {
+    prefix: '/service/',
+    bare: [
+        'https://bare.benusers.repl.co/',
+	'https://bare-server.dev-jc.cf/',
+	'https://tomp.ml/bare/',
+	'https://bare.n-coded.com/',
+	'https://node-ultra.glitch.me/',
+    ],
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
