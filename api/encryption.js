5c48cbb0ef9ccc500fe94e3b7caf70c1b67e28e6
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 19:22:26 -0500

    Improved encryption and decryption with UTF-8 e...

--- api/encryption.js
+++ api/encryption.js
@@ -11,7 +11,7 @@ function generateKey() {
 function encrypt(text, key) {
   const iv = crypto.randomBytes(ivLength);
   const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let encrypted = cipher.update(text);
+  let encrypted = cipher.update(text, 'utf8');
   encrypted = Buffer.concat([encrypted, cipher.final()]);
   const authTag = cipher.getAuthTag();
   return {
@@ -22,14 +22,19 @@ function encrypt(text, key) {
 }
 
 function decrypt(encryptionData, key) {
-  const iv = Buffer.from(encryptionData.iv, 'hex');
-  const encryptedText = Buffer.from(encryptionData.encryptedData, 'hex');
-  const authTag = Buffer.from(encryptionData.authTag, 'hex');
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  decipher.setAuthTag(authTag);
-  let decrypted = decipher.update(encryptedText);
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-  return decrypted.toString();
+  try {
+    const iv = Buffer.from(encryptionData.iv, 'hex');
+    const encryptedText = Buffer.from(encryptionData.encryptedData, 'hex');
+    const authTag = Buffer.from(encryptionData.authTag, 'hex');
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    decipher.setAuthTag(authTag);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString('utf8');
+  } catch (error) {
+    console.error("Decryption failed:", error);
+    return null;
+  }
 }
 
 export { encrypt, decrypt, generateKey };
