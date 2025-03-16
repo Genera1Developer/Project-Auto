928357c920b99693e9508ea12aa0ee1dc132c724
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-15 22:58:31 -0400

    Enhance encryption with JSON stringify/parse ed...

--- api/encryption.js
+++ api/encryption.js
@@ -1,19 +1,19 @@
 const CryptoJS = require('crypto-js');
 
-const secretKey = process.env.ENCRYPTION_KEY || 'SecretPassphrase'; // Use environment variable for key
-const ivKey = process.env.IV_KEY || 'InitializationVe'; // Use environment variable for IV
-const saltRounds = parseInt(process.env.SALT_ROUNDS) || 100; //Use environment variable and parse into Integer
+const secretKey = process.env.ENCRYPTION_KEY || 'SecretPassphrase';
+const ivKey = process.env.IV_KEY || 'InitializationVe';
+const saltRounds = parseInt(process.env.SALT_ROUNDS) || 100;
 
 function encrypt(data) {
     try {
-        const iv = CryptoJS.enc.Utf8.parse(ivKey.substring(0, 16)); // Ensure IV is 16 bytes
-        const salt = CryptoJS.lib.WordArray.random(128/8); // 16 bytes salt
+        const iv = CryptoJS.enc.Utf8.parse(ivKey.substring(0, 16));
+        const salt = CryptoJS.lib.WordArray.random(128/8);
         const key = CryptoJS.PBKDF2(secretKey, salt, {
-            keySize: 256/32,   // 32 bytes
+            keySize: 256/32,
             iterations: saltRounds
         });
 
-        const encrypted = CryptoJS.AES.encrypt(data, key, {
+        const encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), key, { // Encrypt JSON string
             iv: iv,
             mode: CryptoJS.mode.CBC,
             padding: CryptoJS.pad.Pkcs7
@@ -28,12 +28,12 @@ function encrypt(data) {
 
 function decrypt(data) {
     try {
-        const iv = CryptoJS.enc.Utf8.parse(ivKey.substring(0, 16)); // Ensure IV is 16 bytes
+        const iv = CryptoJS.enc.Utf8.parse(ivKey.substring(0, 16));
         const salt = CryptoJS.enc.Hex.parse(data.substring(0, 32));
         const encryptedData = data.substring(32);
 
         const key = CryptoJS.PBKDF2(secretKey, salt, {
-            keySize: 256/32,   // 32 bytes
+            keySize: 256/32,
             iterations: saltRounds
         });
 
@@ -44,7 +44,7 @@ function decrypt(data) {
         });
 
         const decryptedText = decrypted.toString(CryptoJS.enc.Utf8);
-        return decryptedText;
+        return JSON.parse(decryptedText); // Parse JSON string
 
     } catch (error) {
         console.error("Decryption Error:", error);
