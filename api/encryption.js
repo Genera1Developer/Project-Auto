d01fdbd15fbe8c219e03fd32a3d3422c2313e605
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-15 22:19:16 -0400

    Created encryption module with AES and PBKDF2 e...

--- api/encryption.js
+++ api/encryption.js
@@ -1,40 +1,40 @@
 const CryptoJS = require('crypto-js');
 
-const secretPhrase = 'SuperSecretPassphrase';
-const initializationVector = 'InitializationVe'; //Must be exactly 16 bytes
+const secretPhrase = 'MyEncryptionKey123!';
+const ivString = 'MyIVector456!!';
 
-function encrypt(plainText) {
-    const iv = CryptoJS.enc.Utf8.parse(initializationVector);
+function encrypt(text) {
+    const iv = CryptoJS.enc.Utf8.parse(ivString.substring(0, 16));
     const salt = CryptoJS.lib.WordArray.random(128 / 8);
     const key = CryptoJS.PBKDF2(secretPhrase, salt, {
         keySize: 256 / 32,
-        iterations: 500,
+        iterations: 100
     });
 
-    const encrypted = CryptoJS.AES.encrypt(plainText, key, {
+    const encrypted = CryptoJS.AES.encrypt(text, key, {
         iv: iv,
         mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
+        padding: CryptoJS.pad.Pkcs7
     });
 
     return salt.toString() + encrypted.toString();
 }
 
-function decrypt(cipherText) {
+function decrypt(encryptedText) {
     try {
-        const iv = CryptoJS.enc.Utf8.parse(initializationVector);
-        const salt = CryptoJS.enc.Hex.parse(cipherText.substring(0, 32));
-        const encrypted = cipherText.substring(32);
+        const iv = CryptoJS.enc.Utf8.parse(ivString.substring(0, 16));
+        const salt = CryptoJS.enc.Hex.parse(encryptedText.substring(0, 32));
+        const encrypted = encryptedText.substring(32);
 
         const key = CryptoJS.PBKDF2(secretPhrase, salt, {
             keySize: 256 / 32,
-            iterations: 500,
+            iterations: 100
         });
 
         const decrypted = CryptoJS.AES.decrypt(encrypted, key, {
             iv: iv,
             mode: CryptoJS.mode.CBC,
-            padding: CryptoJS.pad.Pkcs7,
+            padding: CryptoJS.pad.Pkcs7
         });
 
         return decrypted.toString(CryptoJS.enc.Utf8);
