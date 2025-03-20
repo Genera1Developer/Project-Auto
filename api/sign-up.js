9d533b79e22547c8b3eb9506caa441039299eeaa
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-20 18:18:37 -0400

    Replaced SHA512 with scrypt for password hashing.

--- api/sign-up.js
+++ api/sign-up.js
@@ -1,15 +1,21 @@
 //sign-up.js
 const crypto = require('crypto');
+const scrypt = require('scrypt-kdf');
 
-function hashPassword(password, salt) {
-  const hash = crypto.createHmac('sha512', salt);
-  hash.update(password);
-  const value = hash.digest('hex');
-  return value;
+const SCRYPT_CONFIG = {
+  cost: 16384,
+  blockSizeFactor: 8,
+  parallelizationFactor: 1,
+  maxMemory: 67108864,
+};
+
+async function hashPassword(password, salt) {
+  const derivedKey = await scrypt.scrypt(password, salt, SCRYPT_CONFIG);
+  return derivedKey.toString('hex');
 }
 
 function generateSalt() {
-  return crypto.randomBytes(16).toString('hex');
+  return crypto.randomBytes(32).toString('hex');
 }
 
 module.exports = async (req, res) => {
@@ -21,7 +27,7 @@ module.exports = async (req, res) => {
     }
 
     const salt = generateSalt();
-    const hashedPassword = hashPassword(password, salt);
+    const hashedPassword = await hashPassword(password, salt);
 
     // Store username, salt, and hashedPassword securely in a database.
     // For demonstration purposes, we'll just log them.
