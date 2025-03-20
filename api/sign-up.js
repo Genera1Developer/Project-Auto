16694e8bbe18e1f4f6b113bb33eb07c5d7b78cbd
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-20 18:21:14 -0400

    Implemented Argon2 hashing and algorithm choice.

--- api/sign-up.js
+++ api/sign-up.js
@@ -1,6 +1,7 @@
 //sign-up.js
 const crypto = require('crypto');
 const scrypt = require('scrypt-kdf');
+const argon2 = require('argon2');
 
 const SCRYPT_CONFIG = {
   cost: 16384,
@@ -9,9 +10,23 @@ const SCRYPT_CONFIG = {
   maxMemory: 67108864,
 };
 
-async function hashPassword(password, salt) {
+async function hashPasswordScrypt(password, salt) {
   const derivedKey = await scrypt.scrypt(password, salt, SCRYPT_CONFIG);
   return derivedKey.toString('hex');
+}
+
+async function hashPasswordArgon2(password) {
+  try {
+    return await argon2.hash(password, {
+      type: argon2.argon2id,
+      memory: 2**16,
+      timeCost: 2,
+      parallelism: 1
+    });
+  } catch (err) {
+    console.error(err);
+    throw new Error('Argon2 hashing failed');
+  }
 }
 
 function generateSalt() {
@@ -20,22 +35,39 @@ function generateSalt() {
 
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    const { username, password } = req.body;
+    const { username, password, hashingAlgo = 'argon2' } = req.body;
 
     if (!username || !password) {
       return res.status(400).json({ message: 'Missing username or password' });
     }
 
-    const salt = generateSalt();
-    const hashedPassword = await hashPassword(password, salt);
+    let hashedPassword, salt = null;
+
+    try {
+      if (hashingAlgo === 'scrypt') {
+        salt = generateSalt();
+        hashedPassword = await hashPasswordScrypt(password, salt);
+      } else if (hashingAlgo === 'argon2'){
+        hashedPassword = await hashPasswordArgon2(password);
+      }
+      else{
+        return res.status(400).json({ message: 'Invalid hashing algorithm'});
+      }
 
     // Store username, salt, and hashedPassword securely in a database.
     // For demonstration purposes, we'll just log them.
-    console.log('Username:', username);
-    console.log('Salt:', salt);
-    console.log('Hashed Password:', hashedPassword);
+      console.log('Username:', username);
+      if (salt){
+        console.log('Salt:', salt);
+      }
+      console.log('Hashed Password:', hashedPassword);
 
-    return res.status(201).json({ message: 'User created successfully' });
+      return res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+      console.error("Error during password hashing:", error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
