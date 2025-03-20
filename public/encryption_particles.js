4fd5de899dab05a2f950bafb39d9d6f7d2452ef9
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-20 17:23:11 -0400

    Added encryption themed particle config

--- public/encryption_particles.js
+++ public/encryption_particles.js
@@ -1,60 +1,64 @@
 const encryptionParticlesConfig = {
   "particles": {
     "number": {
-      "value": 120,
+      "value": 80,
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#00ff00"
+      "value": "#00aaff"
     },
     "shape": {
-      "type": "triangle",
+      "type": "circle",
       "stroke": {
         "width": 0,
         "color": "#000000"
       },
       "polygon": {
         "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
     "opacity": {
-      "value": 0.7,
-      "random": true,
+      "value": 0.5,
+      "random": false,
       "anim": {
-        "enable": true,
-        "speed": 0.5,
-        "opacity_min": 0.3,
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
         "sync": false
       }
     },
     "size": {
-      "value": 4,
+      "value": 5,
       "random": true,
       "anim": {
         "enable": false,
-        "speed": 20,
+        "speed": 40,
         "size_min": 0.1,
         "sync": false
       }
     },
     "line_linked": {
       "enable": true,
-      "distance": 160,
-      "color": "#00ff00",
-      "opacity": 0.5,
-      "width": 2
+      "distance": 150,
+      "color": "#00aaff",
+      "opacity": 0.4,
+      "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 4,
+      "speed": 6,
       "direction": "none",
-      "random": true,
+      "random": false,
       "straight": false,
       "out_mode": "out",
-      "bounce": false,
       "attract": {
         "enable": false,
         "rotateX": 600,
@@ -77,7 +81,7 @@ const encryptionParticlesConfig = {
     },
     "modes": {
       "grab": {
-        "distance": 150,
+        "distance": 140,
         "line_linked": {
           "opacity": 1
         }
@@ -86,12 +90,11 @@ const encryptionParticlesConfig = {
         "distance": 400,
         "size": 40,
         "duration": 2,
-        "opacity": 0.8,
+        "opacity": 8,
         "speed": 3
       },
       "repulse": {
-        "distance": 200,
-        "duration": 0.4
+        "distance": 200
       },
       "push": {
         "particles_nb": 4
@@ -104,10 +107,10 @@ const encryptionParticlesConfig = {
   "retina_detect": true,
   "config_demo": {
     "hide_card": false,
-    "background_color": "#000000",
+    "background_color": "#b61924",
     "background_image": "",
     "background_position": "50% 50%",
     "background_repeat": "no-repeat",
     "background_size": "cover"
   }
-};
+}
