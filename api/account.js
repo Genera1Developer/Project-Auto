ce1bea45a5ffbde8f7939b99bddfe0b6ca6ba3cf
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-20 18:19:20 -0400

    Added AES encryption for username for enhanced ...

--- api/account.js
+++ api/account.js
@@ -1,9 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 const crypto = require('crypto');
 const { promisify } = require('util');
+const { createCipheriv, createDecipheriv } = crypto;
 
 const dbPath = './api/accounts.db'; // Explicit path
 let db;
+
+const encryptionKey = crypto.randomBytes(32); // 256-bit key
+const ivLength = 16; // IV length for AES
 
 function connectToDatabase() {
     db = new sqlite3.Database(dbPath, (err) => {
@@ -50,13 +54,36 @@ function generateSalt() {
     return crypto.randomBytes(32).toString('hex'); // Increased salt size
 }
 
+function encrypt(text) {
+    const iv = crypto.randomBytes(ivLength);
+    const cipher = createCipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
+    let encrypted = cipher.update(text);
+
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+function decrypt(text) {
+    const textParts = text.split(':');
+    const iv = Buffer.from(textParts.shift(), 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    const decipher = createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
+    let decrypted = decipher.update(encryptedText);
+
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted.toString();
+}
+
 
 exports.createUser = async (username, password, callback) => {
     const salt = generateSalt();
     try {
         const { hashedPassword, iterations } = await hashPassword(password, salt);
+        const encryptedUsername = encrypt(username);
 
-        db.run(`INSERT INTO users (username, password, salt, password_version) VALUES (?, ?, ?, ?)`, [username, hashedPassword, salt, iterations], function(err) {
+        db.run(`INSERT INTO users (username, password, salt, password_version) VALUES (?, ?, ?, ?)`, [encryptedUsername, hashedPassword, salt, iterations], function(err) {
             if (err) {
                 console.error(err.message);
                 return callback(err);
@@ -71,7 +98,7 @@ exports.createUser = async (username, password, callback) => {
 
 
 exports.verifyUser = (username, password, callback) => {
-    db.get(`SELECT id, username, password, salt, password_version FROM users WHERE username = ?`, [username], async (err, row) => {
+    db.get(`SELECT id, username, password, salt, password_version FROM users WHERE username = ?`, [encrypt(username)], async (err, row) => {
         if (err) {
             console.error(err.message);
             return callback(err);
@@ -83,7 +110,7 @@ exports.verifyUser = (username, password, callback) => {
         try {
             const { hashedPassword } = await hashPassword(password, row.salt, row.password_version);
             if (hashedPassword === row.password) {
-                callback(null, { id: row.id, username: row.username });
+                callback(null, { id: row.id, username: decrypt(row.username) });
             } else {
                 callback(null, false); // Incorrect password
             }
