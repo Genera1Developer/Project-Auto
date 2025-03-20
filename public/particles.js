de82c8001d61b7a9e782f53630bccfbf648b24dc
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-20 16:33:10 -0400

    Updated particles.js for a cooler theme -END OF...

--- public/particles.js
+++ public/particles.js
@@ -1,4 +1,3 @@
-/* particlesJS v2.0.0 by Vincent Garreau - vincentgarreau.com - MIT license */
 !(function (e, t) {
   "object" == typeof exports && "object" == typeof module
     ? (module.exports = t())
@@ -12,8 +11,8 @@
     "use strict";
     var e = {
         particles: {
-          number: { value: 80, density: { enable: !0, value_area: 800 } },
-          color: { value: "#00ffff" },
+          number: { value: 100, density: { enable: !0, value_area: 800 } },
+          color: { value: "#2ecc71" },
           shape: {
             type: "circle",
             stroke: { width: 0, color: "#000000" },
@@ -21,29 +20,30 @@
             image: { src: "img/github.svg", width: 100, height: 100 },
           },
           opacity: {
-            value: 0.5,
-            random: !1,
-            anim: { enable: !1, speed: 1, opacity_min: 0.1, sync: !1 },
+            value: 0.7,
+            random: !0,
+            anim: { enable: !1, speed: 1, opacity_min: 0.3, sync: !1 },
           },
           size: {
-            value: 5,
+            value: 3,
             random: !0,
             anim: { enable: !1, speed: 40, size_min: 0.1, sync: !1 },
           },
           line_linked: {
             enable: !0,
             distance: 150,
-            color: "#00ffff",
-            opacity: 0.4,
+            color: "#3498db",
+            opacity: 0.6,
             width: 1,
           },
           move: {
             enable: !0,
-            speed: 6,
+            speed: 3,
             direction: "none",
-            random: !1,
+            random: !0,
             straight: !1,
             out_mode: "out",
+            bounce: !1,
             attract: { enable: !1, rotateX: 600, rotateY: 1200 },
           },
         },
@@ -55,22 +55,14 @@
             resize: !0,
           },
           modes: {
-            grab: { distance: 200, line_linked: { opacity: 1 } },
+            grab: { distance: 150, line_linked: { opacity: 1 } },
             bubble: { distance: 400, size: 40, duration: 2, opacity: 0.8, speed: 3 },
-            repulse: { distance: 200 },
+            repulse: { distance: 200, duration: 0.4 },
             push: { particles_nb: 4 },
             remove: { particles_nb: 2 },
           },
         },
         retina_detect: !0,
-        config_demo: {
-          hide_card: !1,
-          background_color: "#b61924",
-          background_image: "",
-          background_position: "50% 50%",
-          background_repeat: "no-repeat",
-          background_size: "cover",
-        },
       },
       t = function () {
         if (window.particlesJS)
