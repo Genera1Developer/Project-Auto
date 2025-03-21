423a57d0812407778c79e7a7ea3915ba75c06061
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-21 02:03:54 -0400

    Added base64 conversion utils for crypto keys/IVs

--- public/encryption_particles.js
+++ public/encryption_particles.js
@@ -280,6 +280,25 @@ particlesJS('particles-js', {
             window.crypto.getRandomValues(iv);
             const ivString = btoa(String.fromCharCode(...iv));
             return ivString;
+        },
+        arrayBufferToBase64: function(buffer) {
+          let binary = '';
+          const bytes = new Uint8Array(buffer);
+          const len = bytes.byteLength;
+          for (let i = 0; i < len; i++) {
+              binary += String.fromCharCode(bytes[i]);
+          }
+          return btoa(binary);
+        },
+
+        base64ToArrayBuffer: function(base64) {
+          const binary_string = atob(base64);
+          const len = binary_string.length;
+          const bytes = new Uint8Array(len);
+          for (let i = 0; i < len; i++) {
+              bytes[i] = binary_string.charCodeAt(i);
+          }
+          return bytes.buffer;
         }
   },
   "fn": {
