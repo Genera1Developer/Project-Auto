e8b1c20f8e800fab68baceb6e0506cbd6d3f10db
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-20 20:35:44 -0400

    Enhanced particle appearance and movement

--- public/encryption_particles.js
+++ public/encryption_particles.js
@@ -1,23 +1,23 @@
 particlesJS('particles-js', {
   particles: {
     number: {
-      value: 120,
+      value: 150,
       density: {
         enable: true,
-        value_area: 700
+        value_area: 800
       }
     },
     color: {
       value: '#00FF00'
     },
     shape: {
-      type: 'polygon',
+      type: 'circle',
       stroke: {
-        width: 0,
+        width: 1,
         color: '#004000'
       },
       polygon: {
-        nb_sides: 5
+        nb_sides: 6
       },
       image: {
         src: '',
@@ -26,35 +26,35 @@ particlesJS('particles-js', {
       }
     },
     opacity: {
-      value: 0.8,
+      value: 0.9,
       random: true,
       anim: {
         enable: true,
-        speed: 0.8,
-        opacity_min: 0.3,
+        speed: 1,
+        opacity_min: 0.4,
         sync: false
       }
     },
     size: {
-      value: 2.5,
+      value: 3,
       random: true,
       anim: {
         enable: true,
-        speed: 8,
+        speed: 10,
         size_min: 0.1,
         sync: false
       }
     },
     line_linked: {
       enable: true,
-      distance: 150,
-      color: '#00B000',
-      opacity: 0.5,
-      width: 1
+      distance: 160,
+      color: '#00C000',
+      opacity: 0.6,
+      width: 2
     },
     move: {
       enable: true,
-      speed: 1.5,
+      speed: 2,
       direction: 'none',
       random: true,
       straight: false,
@@ -81,24 +81,24 @@ particlesJS('particles-js', {
     },
     modes: {
       grab: {
-        distance: 140,
+        distance: 150,
         line_linked: {
-          opacity: 0.7
+          opacity: 0.8
         }
       },
       bubble: {
-        distance: 350,
-        size: 35,
+        distance: 400,
+        size: 40,
         duration: 0.3,
-        opacity: 7,
+        opacity: 8,
         speed: 3
       },
       repulse: {
-        distance: 90,
+        distance: 100,
         duration: 0.4
       },
       push: {
-        particles_nb: 3
+        particles_nb: 4
       },
       remove: {
         particles_nb: 2
