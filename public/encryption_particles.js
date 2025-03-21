31cf704bca34e2057b702d76f06cd520d4a68d37
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-21 05:01:37 -0400

    Added throttling and some utils

--- public/encryption_particles.js
+++ public/encryption_particles.js
@@ -459,6 +459,26 @@ particlesJS('particles-js', {
                     }
                 }
             }
+      },
+      throttle: function(func, limit) {
+          let lastFunc;
+          let lastRan;
+          return function() {
+              const context = this;
+              const args = arguments;
+              if (!lastRan) {
+                  func.apply(context, args);
+                  lastRan = Date.now();
+              } else {
+                  clearTimeout(lastFunc);
+                  lastFunc = setTimeout(function() {
+                      if ((Date.now() - lastRan) >= limit) {
+                          func.apply(context, args);
+                          lastRan = Date.now();
+                      }
+                  }, limit - (Date.now() - lastRan));
+              }
+          }
       }
   },
   "fn": {
