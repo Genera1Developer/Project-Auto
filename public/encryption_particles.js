cf0ff7346e676c1a16992a05689dfa3d02869d6e
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-23 13:18:17 -0400

    Added session/local storage and password deriva...

--- public/encryption_particles.js
+++ public/encryption_particles.js
@@ -762,6 +762,26 @@ particlesJS('particles-js', {
                 }
             }
         },
+        throttleUpdate: function(func, limit) {
+            let lastFunc;
+            let lastRan;
+            return function() {
+                const context = this;
+                const args = arguments;
+                if (!lastRan) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                } else {
+                    clearTimeout(lastFunc);
+                    lastFunc = setTimeout(function() {
+                        if ((Date.now() - lastRan) >= limit) {
+                            func.apply(context, args);
+                            lastRan = Date.now();
+                        }
+                    }, limit - (Date.now() - lastRan));
+                }
+            }
+        },
   },
   "fn": {
     "update": async function() {
