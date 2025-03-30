9255c87308839d4a51392340d94be201fc24e6d7
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 19:38:34 -0400

    Config updated to use env vars
    edit filepath: ...

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 const config = {
-  clientId: 'MUST BE DYNAMIC GRABBING WITH AUTH.JS',
-  clientSecret: 'MUST BE DYNAMIC GRABBING WITH AUTH.JS',
-  redirectUri: 'MUST BE DYNAMIC GRABBING WITH AUTH.JS'
+  githubClientId: process.env.GITHUB_CLIENT_ID || 'YOUR_GITHUB_CLIENT_ID',
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET || 'YOUR_GITHUB_CLIENT_SECRET',
+  githubRedirectUri: process.env.GITHUB_REDIRECT_URI || 'http://localhost:3000/api/auth/callback',
+  apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3000/api'
 };
 
 module.exports = config;
