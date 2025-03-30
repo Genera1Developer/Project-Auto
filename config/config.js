3756fd6ec500e8411b9ee4aa1ffee8388126041c
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 17:11:54 -0400

    Configure GitHub OAuth credentials
    edit filepa...

--- config/config.js
+++ config/config.js
@@ -1,1 +1,7 @@
+const config = {
+  clientId: 'YOUR_GITHUB_APP_CLIENT_ID',
+  clientSecret: 'YOUR_GITHUB_APP_CLIENT_SECRET',
+  redirectUri: 'YOUR_GITHUB_APP_REDIRECT_URI'
+};
 
+module.exports = config;
