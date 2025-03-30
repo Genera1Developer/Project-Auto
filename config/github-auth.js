77e6af1b4831b8cf734ec84dacd5ec20679d9069
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 17:09:29 -0400

    Initial GitHub Auth config file
    edit filepath:...

--- config/github-auth.js
+++ config/github-auth.js
@@ -1,1 +1,6 @@
-
+// This file will contain the GitHub authentication configuration
+module.exports = {
+  clientID: 'YOUR_GITHUB_CLIENT_ID',
+  clientSecret: 'YOUR_GITHUB_CLIENT_SECRET',
+  callbackURL: '/auth/github/callback'
+};
