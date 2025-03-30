86e8d4958be82601e7fb33213764261894cc785d
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 16:57:30 -0400

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -1,1 +1,1 @@
-// connecting github account to the site for it to work
+// This script should connect the users github account to the website through actual github, and once that happens it gets a token from the user to access their account, of which MUST have access to all repositories and to edit and delete and add them.  The token will be used in AI.py
