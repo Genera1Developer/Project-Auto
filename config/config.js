63e040f40d60fce0d9a20eab888c62085cd723a5
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 21:47:43 -0400

    Initial config with GitHub OAuth settings
    edit...

--- config/config.js
+++ config/config.js
@@ -1,1 +1,9 @@
 // must be dynamic from ./api/auth/github.js
+const config = {
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  githubRedirectUri: process.env.GITHUB_REDIRECT_URI || 'http://localhost:3000/api/auth/callback',
+  apiBaseUrl: '/api',
+};
+
+module.exports = config;
