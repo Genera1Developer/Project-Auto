b5ac306e3c77bb3bdddc7678412e1add504ad3a2
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 20:32:54 -0400

    Updated GitHub strategy to include repo scope e...

--- api/auth/github.js
+++ api/auth/github.js
@@ -3,24 +3,22 @@ const GitHubStrategy = require('passport-github2').Strategy;
 const githubAuth = require('../../config/github-auth');
 
 passport.use(new GitHubStrategy({
-  clientID: githubAuth.clientID,
-  clientSecret: githubAuth.clientSecret,
-  callbackURL: githubAuth.callbackURL
+    clientID: githubAuth.clientID,
+    clientSecret: githubAuth.clientSecret,
+    callbackURL: githubAuth.callbackURL,
+    scope: ['repo'] // Request repository access
 },
-function(accessToken, refreshToken, profile, done) {
-  // In a real application, you would likely associate
-  // the GitHub profile with a user record in your database.
-  // For this example, we'll just pass the profile along.
-  profile.accessToken = accessToken;
-  return done(null, profile);
-}));
+    function (accessToken, refreshToken, profile, done) {
+        profile.accessToken = accessToken;
+        return done(null, profile);
+    }));
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 });
 
 module.exports = passport;
