6454b8e268ab2219bad67182c1c12bfbe60f764f
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-31 18:29:11 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,1 @@
-// must be dynamic from ./api/auth/github.js
-const config = {
-  githubClientId: process.env.GITHUB_CLIENT_ID,
-  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
-  githubRedirectUri: process.env.GITHUB_REDIRECT_URI || 'http://localhost:3000/api/auth/callback',
-  apiBaseUrl: '/api',
-};
 
-module.exports = config;
