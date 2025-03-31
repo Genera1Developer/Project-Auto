98503ce739b674242062a7590747d5d67cc8ab59
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-31 18:29:22 -0400

    Update github-auth.js

--- config/github-auth.js
+++ config/github-auth.js
@@ -1,7 +1,1 @@
-const githubAuth = {
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URL || 'https://project-auto-v1029.vercel.app/api/auth/github/callback'
-};
 
-module.exports = githubAuth;
