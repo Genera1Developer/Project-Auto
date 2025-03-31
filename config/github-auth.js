54730e20f8a549e1fea8004710914b14e13721a4
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 21:38:47 -0400

    Configure GitHub authentication settings
    edit ...

--- config/github-auth.js
+++ config/github-auth.js
@@ -1,1 +1,8 @@
 // must be dynamic from ./api/auth/github.js
+const githubAuth = {
+  clientID: 'YOUR_GITHUB_APP_CLIENT_ID',
+  clientSecret: 'YOUR_GITHUB_APP_CLIENT_SECRET',
+  callbackURL: 'https://your-app-url/api/auth/github/callback'
+};
+
+module.exports = githubAuth;
