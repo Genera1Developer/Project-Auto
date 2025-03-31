cdab46ab155bcc8ce9bba8507879af5c9c707085
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 20:06:10 -0400

    Update callback URL to deployed environment edi...

--- config/github-auth.js
+++ config/github-auth.js
@@ -1,5 +1,5 @@
 module.exports = {
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: '/api/auth/github/callback'
+  callbackURL: 'https://project-auto.com/api/auth/github/callback'
 };
