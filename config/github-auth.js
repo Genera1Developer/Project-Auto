4ca224b5d2f47e6b009b895b661b8004c19e05af
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-30 22:52:05 -0400

    Update callback URL to production URL

--- config/github-auth.js
+++ config/github-auth.js
@@ -1,7 +1,7 @@
 const githubAuth = {
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://localhost:3000/api/auth/github/callback'
+  callbackURL: process.env.GITHUB_CALLBACK_URL || 'https://project-auto-v1029.vercel.app/api/auth/github/callback'
 };
 
 module.exports = githubAuth;
